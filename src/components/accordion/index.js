36a12f52a5ebfa85c8cc8672c3d1e12181fcf415
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-21 19:17:40 +0400

    Update index.js

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -54,8 +54,8 @@ Accordion.Header = function AccordionHeader({ children, ...restProps }) {
   );
 };
 
-Accordion.Body = function AccordionBody = ({children, ...restProps}) {
-     const { toggleShow } = useContext(ToggleContext);
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+  const { toggleShow } = useContext(ToggleContext);
 
-     return toggleShow ? <Body {...restProps}>{children}</Body> : null;
-}
+  return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+};
