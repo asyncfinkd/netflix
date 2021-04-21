aa70a6862e0294ab29bed5a76a85493a1f6655e2
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-21 16:22:36 +0400

    Update ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import App from "./app";
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
