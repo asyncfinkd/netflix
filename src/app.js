7ff1b88b40edeb47308bbb8596efbe5845144923
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-21 17:31:14 +0400

    ESLINT Updates.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Jumbotron from "./components/jumbotron";
-import jumboData from "./fixtures/jumbo.json";
+import React from 'react';
+import Jumbotron from './components/jumbotron';
+import jumboData from './fixtures/jumbo.json';
 
 export default function App() {
   return (
