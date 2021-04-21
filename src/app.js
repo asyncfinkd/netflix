53d16137d95e63b6955736809fb2857bdd84ba2b
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-21 17:47:11 +0400

    .env

--- src/app.js
+++ src/app.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import Jumbotron from './components/jumbotron';
-import jumboData from './fixtures/jumbo.json';
+import JumbotronContainer from './components/containers/jumbotron';
 
 export default function App() {
   return (
     <>
-      <Jumbotron.Container>
-        {jumboData.map((item) => {
-          const { id, title, subTitle, image, alt, direction } = item;
-          return (
-            <>
-              <Jumbotron key={id} direction={direction}>
-                <Jumbotron.Pane>
-                  <Jumbotron.Title>{title}</Jumbotron.Title>
-                  <Jumbotron.SubTitle>{subTitle}</Jumbotron.SubTitle>
-                </Jumbotron.Pane>
-                <Jumbotron.Pane>
-                  <Jumbotron.Image src={image} alt={alt} />
-                </Jumbotron.Pane>
-              </Jumbotron>
-            </>
-          );
-        })}
-      </Jumbotron.Container>
+      <JumbotronContainer />
     </>
   );
 }
