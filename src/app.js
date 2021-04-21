add724621faf4a6f3b28400f8e0d67b03d37d8f6
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-21 20:54:31 +0400

    ROUTES

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import JumbotronContainer from './components/containers/jumbotron';
 import FooterContainer from './components/containers/footer';
 import FaqsContainer from './components/containers/faqs';
+import * as ROUTES from './constants/routes';
 
 export default function App() {
   return (
     <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
+      <Router>
+        <Route exact path={ROUTES.HOME}>
+          <JumbotronContainer />
+          <FaqsContainer />
+          <FooterContainer />
+        </Route>
+      </Router>
     </>
   );
 }
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,1 +1,4 @@
 export const HOME = '/';
+export const BROWSE = '/browse';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
