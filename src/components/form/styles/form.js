7f050881e69a0f5f8399969b6ad9d38f0d84a604
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-24 11:55:13 +0400

    stylesheet

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -2,3 +2,16 @@ import styled from "styled-components/macro";
 import { Link as ReactRouterLink } from "react-router-dom";
 
 export const Container = styled.div``;
+export const Error = styled.div``;
+export const Base = styled.div``;
+export const Text = styled.p``;
+export const Title = styled.h1``;
+export const TextSmall = styled.p``;
+export const Link = styled(ReactRouterLink)`
+  color: white;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration-line: underline;
+  }
+`;
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,20 +1,17 @@
 import Firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-import { seedDatabase } from "../seed";
 
 const config = {
-  apiKey: "AIzaSyD6JRu4nIavRDDghojGR-S7sGT5ks2lXBE",
-  authDomain: "netflix-8f0a6.firebaseapp.com",
-  projectId: "netflix-8f0a6",
-  storageBucket: "netflix-8f0a6.appspot.com",
-  messagingSenderId: "292054236499",
-  appId: "1:292054236499:web:76bbded64b0accfd8635a6",
-  measurementId: "G-N1PLBNEX6E",
+  apiKey: "AIzaSyCBS1-8Nlo2uebcL-UyLoCZxs7Y3wuCJrI",
+  authDomain: "netflix-web-df6eb.firebaseapp.com",
+  projectId: "netflix-web-df6eb",
+  storageBucket: "netflix-web-df6eb.appspot.com",
+  messagingSenderId: "241020580474",
+  appId: "1:241020580474:web:29cd2f7c8c10685f7441c3",
+  measurementId: "G-330K8C7GT3",
 };
 
 const firebase = Firebase.initializeApp(config);
-
-seedDatabase(firebase);
 
 export { firebase };
