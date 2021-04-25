4b303d2c4b7d059a9b41e4f94f7482a35599481a
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-25 19:05:46 +0400

    Sign up full Component, We Have SIgn in, Sign up Pages

--- src/components/pages/signup.js
+++ src/components/pages/signup.js
@@ -18,6 +18,26 @@ export default function Signup() {
 
   const handleSignup = (event) => {
     event.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then((result) => {
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          });
+      })
+      .catch((err) => {
+        setFirstName("");
+        setEmailAddress("");
+        setPassword("");
+        setError(err.message);
+      });
   };
 
   return (
@@ -29,16 +49,19 @@ export default function Signup() {
 
           <Form.Base onSubmit={handleSignup} method="POST">
             <Form.Input
+              type="text"
               placeholder="First name"
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
             />
             <Form.Input
+              type="text"
               placeholder="Email address"
               value={emailAddress}
               onChange={(e) => setEmailAddress(e.target.value)}
             />
             <Form.Input
+              type="password"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
