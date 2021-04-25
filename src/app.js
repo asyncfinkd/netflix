b62c327067fcfe1bcd62255907a86d791bffa538
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-25 19:41:22 +0400

    Fix Page Routes

--- src/app.js
+++ src/app.js
@@ -10,36 +10,21 @@ import Feature from "./components/feature/index";
 import SignIn from "./components/pages/signin";
 import Signup from "./components/pages/signup";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import Home from "./components/pages/home";
 
 export default function App() {
-  const user = { name: "karl" };
+  const user = null;
   return (
     <>
       <Router>
-        <Route exact path={ROUTES.HOME}>
-          <HeaderContainer>
-            <Feature>
-              <Feature.Title>
-                Unlimited movies, TV shows, and more.
-              </Feature.Title>
-              <Feature.Subtitle>
-                Watch anywhere. Cancel at anytime.
-              </Feature.Subtitle>
-              <OptForm.Text>
-                Ready to watch? Enter your email to create or restart your
-                membership
-              </OptForm.Text>
-              <OptForm>
-                <OptForm.Input placeholder="Email address" />
-                <OptForm.Button>Get Started</OptForm.Button>
-                <OptForm.Break />
-              </OptForm>
-            </Feature>
-          </HeaderContainer>
-          <JumbotronContainer />
-          <FaqsContainer />
-          <FooterContainer />
-        </Route>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+          exact
+        >
+          <Home />
+        </IsUserRedirect>
         <IsUserRedirect
           user={user}
           loggedInPath={ROUTES.BROWSE}
@@ -59,14 +44,6 @@ export default function App() {
         <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
           <p>browse</p>
         </ProtectedRoute>
-        <IsUserRedirect
-          user={user}
-          loggedInPath={ROUTES.BROWSE}
-          path={ROUTES.HOME}
-          exact
-        >
-          <p>home</p>
-        </IsUserRedirect>
       </Router>
     </>
   );
