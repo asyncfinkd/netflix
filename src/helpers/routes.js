32339a2b2c8c6efb166977a3fd3ddf0489d22560
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-25 19:11:56 +0400

    helpers to user identify isLogged or not

---
+++ src/helpers/routes.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+export default function isUserRedirect({
+  user,
+  loggedInPath,
+  children,
+  ...rest
+}) {
+  return (
+    <>
+      <Route
+        {...rest}
+        render={() => {
+          if (!user) {
+            return children;
+          }
+
+          if (user) {
+            return (
+              <Redirect
+                to={{
+                  pathname: loggedInPath,
+                }}
+              />
+            );
+          }
+
+          return null;
+        }}
+      />
+    </>
+  );
+}
