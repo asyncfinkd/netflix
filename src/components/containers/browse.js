36f8543654b6cbe9bb4a857f1679cee88febdee5
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-25 21:53:19 +0400

    Build Show Movies Page

--- src/components/containers/browse.js
+++ src/components/containers/browse.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext, useState } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from "../../context/firebase";
 import Loading from "../../components/loading/index";
+import Header from "../../components/header/index";
 
 export default function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
@@ -17,9 +18,12 @@ export default function BrowseContainer({ slides }) {
   return (
     <>
       {profile.displayName ? (
-        loading ? (
-          <Loading src={user.photoURL} />
-        ) : null
+        <>
+          {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+          <Header src="joker1">
+            <p>hello</p>
+          </Header>
+        </>
       ) : (
         <SelectProfileContainer user={user} setProfile={setProfile} />
       )}
