e080701e046161c38d9930e11f4d18a427f7a976
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-28 13:29:04 +0400

    :whale2: header and hero. some new features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,46 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>
+          Netflix Georgia - Watch TV Shows Online, Watch Movies Online
+        </title>
+        <link
+          type="text/css"
+          rel="stylesheet"
+          href="/personalization/cl2/freeform/WebsiteDetect?source=wwwhead&amp;fetchType=css&amp;modalView=nmLanding"
+          data-uia="botLink"
+        />
+        <link
+          type="text/css"
+          rel="stylesheet"
+          href="https://codex.nflxext.com/%5E3.0.0/truthBundle/webui/1.22.5-shakti-css-vafe38bd5/css/css/less%7Ccore%7Cerror-page.less/1/btzvF4DuBgayCE/none/true/none"
+          data-uia="botLink"
+        />
+        <link
+          type="text/css"
+          rel="stylesheet"
+          href="https://codex.nflxext.com/%5E3.0.0/truthBundle/webui/1.22.5-shakti-css-vafe38bd5/css/css/less%7Csignup%7Cnmhp%7Cnmhp.less/1/btzvF4DuBgayCE/none/true/none"
+          data-uia="botLink"
+        />
+        <link
+          rel="shortcut icon"
+          href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png"
+        />
+      </Head>
+      <div id="appMountPoint">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from "next";
+import Header from "../ui/header";
+import Hero from "../ui/hero";
 
 const Home: NextPage = () => {
-  return <p>hi</p>;
+  return (
+    <>
+      <div className="basicLayout">
+        <div className="netflix-sans-font-loaded">
+          <div className="our-story-desktop-framework">
+            <div>
+              <div className="lang-selector-test our-story-container" dir="ltr">
+                <Header />
+                <Hero />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
